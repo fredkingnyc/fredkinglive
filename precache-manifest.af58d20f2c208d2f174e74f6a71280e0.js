c47765e50db2e9058f8b5bd857e034d60d33d256
Fred King <fredkingnyc@gmail.com>
2020-04-20 15:18:46 -0700

    Updates

---
+++ precache-manifest.af58d20f2c208d2f174e74f6a71280e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9096b7d7f92d120264bd96a3c093e085",
+    "url": "/fredkinglive/index.html"
+  },
+  {
+    "revision": "99046ab7886eb423bcaa",
+    "url": "/fredkinglive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bb204aa938762c521f7",
+    "url": "/fredkinglive/static/js/2.329532c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fredkinglive/static/js/2.329532c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99046ab7886eb423bcaa",
+    "url": "/fredkinglive/static/js/main.81857483.chunk.js"
+  },
+  {
+    "revision": "444d21f2eb2f40bfe05c",
+    "url": "/fredkinglive/static/js/runtime-main.d8e58bc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fredkinglive/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fredkinglive/precache-manifest.af58d20f2c208d2f174e74f6a71280e0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fredkinglive/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
